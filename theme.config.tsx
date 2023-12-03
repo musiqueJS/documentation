ab82f71f6c04eb2de716edb7263a8928cff8e73e
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-12-03 11:58:32 +0100

    :tada: SETUP documentation structure

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Musique.js</span>,
+  project: {
+    link: 'https://github.com/musiqueJS/musiqueJS',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Musique.js © 2023',
+  },
+}
+
+export default config
