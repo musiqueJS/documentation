165fd95fbd3da53d1b95596255830810b8307588
Nicolas de Garrigues <ndegarrigues@gmail.com>
2023-12-08 14:47:51 +0100

    :construction: FEED documentation introduction, installation, about page & classes

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
   logo: <span>Musique.js</span>,
   project: {
-    link: 'https://github.com/musiqueJS/musiqueJS',
+    link: "https://github.com/musiqueJS/musiqueJS",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Musique.js © 2023',
+    text: "Musique.js © 2023",
   },
-}
+  useNextSeoProps() {
+    const { route } = useRouter();
+    if (route !== "/") {
+      return {
+        titleTemplate: "%s | Musique.js",
+      };
+    } else {
+      return {
+        titleTemplate: "Musique.js",
+      };
+    }
+  },
+  head: (
+    <>
+      <link rel="icon" href="static/logo.png" type="image/svg+xml+png" />
 
-export default config
+      <link
+        rel="icon"
+        href="static/logo.png"
+        type="image/svg+xml+png"
+        media="(prefers-color-scheme: dark)"
+      />
+    </>
+  ),
+};
+
+export default config;
